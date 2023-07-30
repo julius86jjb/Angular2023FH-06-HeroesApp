4fb267d99ef606bb79efa2f270c1562dea3d01fa
Julio Jimenez <julius.1986@gmail.com>
2023-07-30 16:10:38 +0100

    Fin Sección 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
-import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { SharedModule } from './shared/shared.module';
+
+
+
 import { AppComponent } from './app.component';
+
+
 
 
 @NgModule({
@@ -15,8 +20,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-page',
@@ -7,5 +12,38 @@ import { Component } from '@angular/core';
   ]
 })
 export class SearchPageComponent {
+
+  public searchInput = new FormControl('')
+
+  public heroes: Hero[] = []
+
+  public selectedHero?: Hero;
+
+  constructor( private heroesService: HeroesService,
+              private router: Router ) {
+
+  }
+
+  searchHero() {
+    const value: string = this.searchInput.value || '';
+
+    this.heroesService.getSuggestions(value)
+      .subscribe(heroes => this.heroes = heroes);
+
+  }
+  onSelectedOption(event: MatAutocompleteSelectedEvent): void {
+    if (!event.option.value) {
+      this.selectedHero = undefined;
+      return;
+    }
+
+    const hero: Hero = event.option.value
+    this.searchInput.setValue (hero.superhero);
+
+    this.selectedHero = hero;
+
+    // this.router.navigateByUrl(`/heroes/${hero.id}`)
+    this.router.navigate(['heroes', hero.id])
+  }
 
 }
